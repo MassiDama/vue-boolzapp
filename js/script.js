85a2eae5ea067a0f73cf5cfd82b52be206b112f9
Massidama <massidama@outlook.it>
2023-12-14 11:47:24 +0100

    feat: struttura e stili sfondo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+        
+        }
+    },
+    methods: {
+
+
+    },
+    mounted() {
+        console.log("L'applicazione è caricata!");
+
+        };
+       
+
+    
+
+
+}).mount('#app')
